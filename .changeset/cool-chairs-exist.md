53d9e7efc0a5c2c5bb696af1cd527ee12762814a
Logan McAnsh <logan@mcan.sh>
2024-04-02 16:21:48 -0400

    chore: don't add icon to sprite multiple times (#13)

---
+++ .changeset/cool-chairs-exist.md
@@ -0,0 +1,5 @@
+---
+"@mcansh/vite-svg-sprite-plugin": patch
+---
+
+dont add icon to sprite multiple times
--- packages/vite-svg-sprite-plugin/src/index.ts
+++ packages/vite-svg-sprite-plugin/src/index.ts
@@ -6,6 +6,7 @@ import { ResolvedConfig, Plugin } from "vite";
 
 let svgRegex = /\.svg$/;
 let store = svgstore();
+let icons = new Set<string>();
 
 type Options = {
   spriteOutputName?: string;
@@ -45,19 +46,19 @@ export function createSvgSpritePlugin(options?: Options): Plugin {
           .update(content)
           .digest("hex");
 
-        let symbolId: string = symbolIdPattern;
-        if (symbolIdPattern) {
-          if (symbolIdPattern.includes("[name]")) {
-            symbolId = symbolId.replace("[name]", basename);
-          }
-          if (symbolId.includes("[hash]")) {
-            symbolId = symbolId.replace("[hash]", hash);
-          }
-        } else {
-          symbolId = `icon-${basename}-${hash}`;
+        let symbolId = symbolIdPattern;
+        if (symbolIdPattern.includes("[name]")) {
+          symbolId = symbolId.replace("[name]", basename);
+        }
+        if (symbolId.includes("[hash]")) {
+          symbolId = symbolId.replace("[hash]", hash);
         }
 
-        store.add(symbolId, content);
+        // only add the icon if it hasn't been added before
+        if (!icons.has(symbolId)) {
+          store.add(symbolId, content);
+          icons.add(id);
+        }
 
         return {
           code: `export default "${url}#${symbolId}";`,
@@ -69,6 +70,8 @@ export function createSvgSpritePlugin(options?: Options): Plugin {
     async writeBundle() {
       let { assetsDir, outDir } = config.build;
       let sprite = store.toString();
+      console.log(icons.size, "icons added to sprite");
+
       let spritePath = path.join(outDir, assetsDir, spriteOutputName);
       await fse.outputFile(spritePath, sprite);
     },
