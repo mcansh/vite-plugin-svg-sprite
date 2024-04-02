1485b3a0b24b6bff25ccba7b508319d719e23331
Logan McAnsh <logan@mcan.sh>
2024-04-01 23:22:34 -0400

    initial commit
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import path from "node:path";
+import crypto from "node:crypto";
+import fse from "fs-extra";
+import svgstore from "svgstore";
+import { ResolvedConfig, Plugin } from "vite";
+
+let svgRegex = /\.svg$/;
+let store = svgstore();
+
+export function createSpriteFromImportedSvgs({
+  spriteOutputName = "sprite.svg",
+  symbolId: symbolIdPattern,
+}: {
+  spriteOutputName?: string;
+  symbolId?: string;
+} = {}): Plugin {
+  let config: ResolvedConfig;
+
+  return {
+    name: "create-sprite-from-imported-svgs",
+
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
+
+    async transform(_code, id) {
+      if (svgRegex.test(id)) {
+        let basename = path.basename(id, ".svg");
+        if (basename === "sprite") return null;
+        let content = await fse.readFile(id, "utf-8");
+
+        let hash = crypto
+          .createHash("shake256", { outputLength: 4 })
+          .update(content)
+          .digest("hex");
+
+        let symbolId: string;
+        if (symbolIdPattern) {
+          if (symbolIdPattern.includes("[name]")) {
+            symbolIdPattern = symbolIdPattern.replace("[name]", basename);
+          }
+          if (symbolIdPattern.includes("[hash]")) {
+            symbolIdPattern = symbolIdPattern.replace("[hash]", hash);
+          }
+          symbolId = symbolIdPattern;
+        } else {
+          symbolId = `icon-${basename}-${hash}`;
+        }
+
+        store.add(symbolId, content);
+
+        return {
+          code: `export default "/${config.build.assetsDir}/${spriteOutputName}#${symbolId}";`,
+          map: null,
+        };
+      }
+    },
+
+    async writeBundle() {
+      let { assetsDir, outDir } = config.build;
+      let sprite = store.toString();
+      let spritePath = path.join(outDir, assetsDir, spriteOutputName);
+      await fse.outputFile(spritePath, sprite);
+    },
+
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === `/assets/${spriteOutputName}`) {
+          res.setHeader("Content-Type", "image/svg+xml");
+          res.end(store.toString());
+        } else {
+          next();
+        }
+      });
+    },
+  };
+}
