2328552da2b241a5d75278465a8b53840dd8cefa
Logan McAnsh <logan@mcan.sh>
2024-04-02 14:24:23 -0400

    feat: allow plugin to work with custom assetDir
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- examples/vite-no-remix/vite.config.ts
+++ examples/vite-no-remix/vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import { createSvgSpritePlugin } from "@mcansh/vite-svg-sprite-plugin";
 
 export default defineConfig({
+  build: { assetsDir: "something-other-than-assets" },
   plugins: [react(), createSvgSpritePlugin()],
 });
