c6884029009ecdba3e74619a88c752cb78a1177f
Logan McAnsh <logan@mcan.sh>
2024-04-10 18:23:19 -0400

    feat: re-add virtual module
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- examples/remix/app/routes/_index/route.tsx
+++ examples/remix/app/routes/_index/route.tsx
@@ -3,6 +3,7 @@ import archiveBoxArrowDownIconHref from "~/assets/archive-box-arrow-down.svg";
 import beakerIconHref from "heroicons/24/outline/beaker.svg";
 import eyeIconHref from "lucide-static/icons/eye.svg";
 import homeIconHref from "lucide-static/icons/home.svg";
+import spriteUrl from "virtual:@mcansh/vite-svg-sprite-plugin";
 import { Icon } from "./icon";
 
 export const meta: MetaFunction = () => {
@@ -16,6 +17,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <h2>{spriteUrl}</h2>
 
       <Icon icon={archiveBoxArrowDownIconHref} width={100} height={100} />
       <Icon icon={beakerIconHref} width={100} height={100} />
