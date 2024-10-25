2d0e91d93a9e5818d1a98924fc8237022d0c9617
Logan McAnsh <logan@mcan.sh>
2024-10-25 16:30:13 -0400

    chore: cjs
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ packages/vite-plugin-svg-sprite/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  clean: true,
+  dts: true,
+  outDir: "dist",
+  tsconfig: "tsconfig.json",
+})
